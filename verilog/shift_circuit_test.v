a65d7b772472f7a344ea82f7a5088d1684319ca5
Lolme22 <145405117+Lolme22@users.noreply.github.com>
2023-09-21 02:52:04 -0500

    Update shift_circuit_test.v

--- verilog/shift_circuit_test.v
+++ verilog/shift_circuit_test.v
@@ -4,21 +4,20 @@ module shift_circuit_tb;		//module name
   reg clk = 0;
   always #5 clk = !clk;			//every 5 ns clk either 1 or 0S
   
-  reg [3:0] a;				//inputs
-  reg [1:0] shift;
+  reg [3:0] a;				//input
 
-  initial begin				//load new values
-    	 a = 4'b0010; shift = 2'b00;	//every 10 nano
-    # 10 a = 4'b1000; shift = 2'b01;	//seconds
-    # 10 a = 4'b1111; shift = 2'b10;
-    # 10 a = 4'b1001; shift = 2'b11;
+  initial begin				        //load new values
+         a = 4'b0010;         //every 10 nano
+    # 10 a = 4'b1000;         //seconds
+    # 10 a = 4'b1111;
+    # 10 a = 4'b1001;
     # 10 $finish;
   end
 
   wire [3:0]l_shift;				//outputs
   wire [3:0]r_shift;
   
-  shift_4bit a0(a, shift, l_shift, r_shift);  //call for gate module
+  shift_4bit a0(a, l_shift, r_shift);  //call for gate module
   
   initial begin				//code for gtkwave
     $dumpfile("shift_circuit.vcd");
@@ -26,11 +25,10 @@ module shift_circuit_tb;		//module name
     $dumpvars(1,a);
     $dumpvars(2,l_shift);
     $dumpvars(3,r_shift);
-    $dumpvars(4,shift);
   end
 
   initial 				//prints to screen when values change
-    $monitor("At time %t, a(%b) << l_shift(%b) = (%b), a(%b) >> r_shift(%b) = (%b)",  
-    		$time, a, shift, l_shift, a, shift, r_shift);   
+    $monitor("At time %t, a(%b) << 1 = (%b), a(%b) >> 1 = (%b)",  
+    		$time, a, l_shift, a, r_shift);   
  									
 endmodule //test
